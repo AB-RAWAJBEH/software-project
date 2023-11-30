bee091e7cac27ead283fb3f570fd2a53c76d6c8f
abeer <abeer@192.168.1.26>
2023-11-30 20:31:49 +0200

    .

--- Software-project/src/main/java/logout.java
+++ Software-project/src/main/java/logout.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.assertEquals;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.*;
+
 
 public class logout {
 	
